a1a3aa8d413cbfe3b76cdbe9496a91ee42252928
Arsyad Arthan N <49345220+arsyad1706@users.noreply.github.com>
2024-05-28 20:10:30 +0700

    fix: status schedule

--- resources/views/admin/schedule/_status.blade.php
+++ resources/views/admin/schedule/_status.blade.php
@@ -2,7 +2,7 @@
     <span class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">Hari Ini</span>
 @elseif ($status == 'upcoming')
     <span
-        class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">{{ $schedule->date->format('d M Y') }}</span>
+        class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">{{ $status }}</span>
 @elseif ($status == 'done')
     <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">Selesai</span>
 @endif
